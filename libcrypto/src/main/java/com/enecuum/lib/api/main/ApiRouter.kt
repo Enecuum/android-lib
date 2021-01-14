f94a248e9c027398cb9b9ebc68df955ccaafba15
sdfi1984 <mamoncillo@protonmail.com>
2021-01-14 19:29:14 +0300

    Misc

--- libcrypto/src/main/java/com/enecuum/lib/api/main/ApiRouter.kt
+++ libcrypto/src/main/java/com/enecuum/lib/api/main/ApiRouter.kt
@@ -33,8 +33,8 @@ object ApiRouter {
     }
 
     //TODO SSL/TLS please
-    val wsProtocolPrefix = "ws://"
     val httpProtocolPrefix = "https://"
+    val wsProtocolPrefix = "ws://"
 
     val wsURL = wsProtocolPrefix + setter.ip + ":" + setter.PortWs
     val apiURL = httpProtocolPrefix + setter.domain + ":" + setter.PortHttp + setter.apiSuffix
